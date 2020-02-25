1570566e09a5323ed6da849fc4dd5825669211e1
RamziBach <ramzibach.dev@gmail.com>
2020-02-25 01:06:30 -0500

    Website

---
+++ script.js
@@ -0,0 +1,22 @@
+const hamburger = document.querySelectorAll('.hamburger');
+const mobileNav = document.querySelector('.hamburger-container');
+const clear = document.querySelector('.clear');
+const containerSticky = document.querySelector('.sticky-header');
+const closeNav = document.querySelectorAll('.hamburger-li');
+
+hamburger[0].addEventListener('click', () => mobileNav.style.display = 'block');
+hamburger[1].addEventListener('click', () => mobileNav.style.display = 'block');
+clear.addEventListener('click', () => mobileNav.style.display = 'none');
+closeNav[0].addEventListener('click', () => mobileNav.style.display = 'none');
+closeNav[1].addEventListener('click', () => mobileNav.style.display = 'none');
+closeNav[2].addEventListener('click', () => mobileNav.style.display = 'none');
+
+window.onscroll = () => scrollFunction();
+
+const scrollFunction = () => {
+ 	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		containerSticky.style.display = "block";
+  	} else {
+		containerSticky.style.display = "none";
+  	}
+};
